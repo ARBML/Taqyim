076bb714a3e94abc61d60c64abf91f28829c7f91
zaidalyafeai <alyafey22@gmail.com>
2023-06-11 00:40:56 +0300

    refactor the results and add prompt engineering results

---
+++ prompt_engineering.py
@@ -0,0 +1,26 @@
+import taqyim as tq
+
+prompts = [
+    'Predict the sentiment of the following statement in English: choose an option: Positive , Negative',
+    'Is the sentiment of the following statement Positive or Negative?',
+    'ماهي عاطفة الجملة التالية : أجب Positive أو Negative',
+    'You are a helpful assistant that can predict whether a given statement in Arabic is Positive or Negative'
+]
+for i, prompt in enumerate(prompts):
+
+    pipeline = tq.Pipeline(
+        eval_name = f"ajgt-prompt-gpt-4-{i}",
+        dataset_name="arbml/ajgt_ubc_split",
+        task_class= "classification",
+        task_desc = "Arabic text sentiment analysis",
+        input_column_name = 'content',
+        target_column_name = 'label',
+        prompt=prompt,
+        api_key='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',
+        train_split="train",
+        test_split="test",
+        model_name = "gpt-4-0314",
+        max_samples= 360,)
+
+    pipeline.run()
+    print(pipeline.show_results())
